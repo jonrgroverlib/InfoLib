cae726afe911f3052e991f7bd56431a8ce1f9071
jonrgrover <34585315+jonrgroverlib@users.noreply.github.com>
2019-04-12 12:09:05 -0400

    Add files via upload

---
+++ Models/EndemeTable.cs
@@ -0,0 +1,63 @@
+﻿//--------------------------------------------------------------------------------------------------
+// This file is part of the InfoLibCsLesserGpl version of Informationlib.
+//
+// InfoLib is free software: you can redistribute it and/or modify it under the terms
+// of the GNU Lesser General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// InfoLib is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with
+// InfoLib. If not, see <http://www.gnu.org/licenses/>.
+//--------------------------------------------------------------------------------------------------
+using InfoLib.Endemes;
+using System;
+using System.Collections.Generic;
+
+namespace InfoLib.Models
+{
+    // --------------------------------------------------------------------------------------------
+    /// <!-- EndemeTable -->
+    /// <summary>
+    ///      The EndemeTable class contains objects that parallel the Endeme table
+    /// </summary>
+    public class EndemeTable
+    {
+        // ----------------------------------------------------------------------------------------
+        //  Members
+        // ----------------------------------------------------------------------------------------
+        //[Key]
+        public long      EndemeId        { get; set; } // primary key
+        public Guid      EndemeSetId     { get; set; }
+        public string    EndemeString    { get; set; }
+        public string    EndemeCode      { get; set; }
+        public string    EndemeLabel     { get; set; }
+        public string    EndemeDescr     { get; set; }
+        public bool      RawSource       { get; set; } // not null
+        public bool      MultipleUse     { get; set; } // not null
+        public byte[]    ValueBinary     { get { return _valueBinary; } set { _valueBinary = value; } }  public byte[] _valueBinary;
+        public DateTime? ValueDateTime   { get; set; }
+        public double    ValueFloat      { get; set; }
+        public decimal   ValueNumber     { get; set; }
+        public string    ValueText       { get; set; }
+        public int       EndemeProfileId { get; set; }
+        public EndemeSet EndemeSet       { get; set; }
+        public List<EndemeIndex> EndemeIndexList { get; set; }
+
+
+        // ----------------------------------------------------------------------------------------
+        //  Constructor
+        // ----------------------------------------------------------------------------------------
+        public EndemeTable()
+        {
+        }
+
+        public override string ToString()
+        {
+            return EndemeString + " " + EndemeId + " " + EndemeLabel + " " + ValueText + ValueNumber;
+        }
+    }
+
+}
---
+++ Models/EndemeUsage.cs
@@ -0,0 +1,40 @@
+﻿using InfoLib.Endemes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations; // for [Key]
+using System.Linq;
+using System.Text;
+
+namespace InfoLib.Models
+{
+    // --------------------------------------------------------------------------------------------
+    /// <!-- EndemeUsage -->
+    /// <summary>
+    ///      The EndemeUsage class contains objects that parallel the EndemeUsage table
+    /// </summary>
+    public class EndemeUsage
+    {
+        // ----------------------------------------------------------------------------------------
+        //  Members
+        // ----------------------------------------------------------------------------------------
+        [Key]
+        public int       EndemeUsageId       { get; set; }  // primary key
+        public Guid      EndemeSetId         { get; set; }
+        public string    TableName           { get; set; }
+        public string    TableEndemeFkColumn { get; set; }
+        public string    TablePkColumn       { get; set; }
+        public string    TableRowLabel       { get; set; }
+        public string    ExtractLabelB       { get; set; }
+        public EndemeSet EndemeSet           { get; set; }
+
+
+        // ----------------------------------------------------------------------------------------
+        //  Constructor
+        // ----------------------------------------------------------------------------------------
+        public EndemeUsage()
+        {
+        }
+
+    }
+
+}
